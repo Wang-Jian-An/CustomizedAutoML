6425c56191ae467ad85811a7ead624051198e31b
Jian-An Wang <facebook0983481181@gmail.com>
2023-01-24 23:39:53 +0800

    fix: modify combination mode

--- kernelFunction.py
+++ kernelFunction.py
@@ -7,10 +7,10 @@ def polynomial_kernel_function_with_degree_two(one_data: np.ndarray,
     
     # 確認 x, y 皆為一維向量且長度相同
     assert np.ndim(one_data) == 2, "The dimension of x must be 1. "
-    return np.hstack([
-        np.power(one_data, 2),
-        np.array([np.cumprod(one_data[:, [i, j]], axis = 0)[:, -1] for i, j in combinations(list(range(one_data.shape[1])), 2)]).T
-    ])
+    return np.array([
+        np.power(one_data, 2).T.tolist(),
+        np.array([np.cumprod(one_data[:, [i, j]], axis = 0)[:, -1] for i, j in combinations(list(range(one_data.shape[1])), 2)]).tolist()
+    ]).T
 
 def executeKernelFunc(data: pd.DataFrame, 
                       inputFeatures: list):
