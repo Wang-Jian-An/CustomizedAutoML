a1889c2fadc19805c8476129a57558bde2398098
Jian-An Wang <facebook0983481181@gmail.com>
2023-03-09 19:04:42 +0800

    style: format

--- two_class_model_evaluation.py
+++ two_class_model_evaluation.py
@@ -3,38 +3,43 @@ import pandas as pd
 
 from sklearn.metrics import *
 
+
 def model_evaluation(ytrue: np.array, ypred: np.array, ypred_proba: np.array):
     # F1-Score
-    f1_score_1 = f1_score(y_true = ytrue, y_pred = ypred, pos_label = 1)
-    f1_score_0 = f1_score(y_true = ytrue, y_pred = ypred, pos_label = 0)
-    macro_f1_score = f1_score(y_true = ytrue, y_pred = ypred, average = "macro")
-    micro_f1_score = f1_score(y_true = ytrue, y_pred = ypred, average = "micro")
+    f1_score_1 = f1_score(y_true=ytrue, y_pred=ypred, pos_label=1)
+    f1_score_0 = f1_score(y_true=ytrue, y_pred=ypred, pos_label=0)
+    macro_f1_score = f1_score(y_true=ytrue, y_pred=ypred, average="macro")
+    micro_f1_score = f1_score(y_true=ytrue, y_pred=ypred, average="micro")
 
     # PRC-AUC
-    prc_precision_1, prc_recall_1, prc_threshold_1 = precision_recall_curve(y_true = ytrue, probas_pred = ypred_proba, pos_label = 1)
-    prc_precision_0, prc_recall_0, prc_threshold_0 = precision_recall_curve(y_true = ytrue, probas_pred = ypred_proba, pos_label = 0)
+    prc_precision_1, prc_recall_1, prc_threshold_1 = precision_recall_curve(
+        y_true=ytrue, probas_pred=ypred_proba, pos_label=1
+    )
+    prc_precision_0, prc_recall_0, prc_threshold_0 = precision_recall_curve(
+        y_true=ytrue, probas_pred=ypred_proba, pos_label=0
+    )
 
     prc_auc_1 = auc(prc_recall_1, prc_precision_1)
     prc_auc_0 = auc(prc_recall_0, prc_precision_0)
 
-    # Precision 
-    precision_0 = precision_score(y_true = ytrue, y_pred = ypred, pos_label = 0)
-    precision_1 = precision_score(y_true = ytrue, y_pred = ypred, pos_label = 1)
-    macro_precision = precision_score(y_true = ytrue, y_pred = ypred, average = "macro")
-    micro_precision = precision_score(y_true = ytrue, y_pred = ypred, average = "micro")
+    # Precision
+    precision_0 = precision_score(y_true=ytrue, y_pred=ypred, pos_label=0)
+    precision_1 = precision_score(y_true=ytrue, y_pred=ypred, pos_label=1)
+    macro_precision = precision_score(y_true=ytrue, y_pred=ypred, average="macro")
+    micro_precision = precision_score(y_true=ytrue, y_pred=ypred, average="micro")
 
     # Recall
-    recall_0 = recall_score(y_true = ytrue, y_pred = ypred, pos_label = 0)
-    recall_1 = recall_score(y_true = ytrue, y_pred = ypred, pos_label = 1)
-    macro_recall = recall_score(y_true = ytrue, y_pred = ypred, average = "macro")
-    micro_recall = recall_score(y_true = ytrue, y_pred = ypred, average = "micro")
-    
+    recall_0 = recall_score(y_true=ytrue, y_pred=ypred, pos_label=0)
+    recall_1 = recall_score(y_true=ytrue, y_pred=ypred, pos_label=1)
+    macro_recall = recall_score(y_true=ytrue, y_pred=ypred, average="macro")
+    micro_recall = recall_score(y_true=ytrue, y_pred=ypred, average="micro")
+
     #  Accuracy
-    accuracy = accuracy_score(y_true = ytrue, y_pred = ypred)
+    accuracy = accuracy_score(y_true=ytrue, y_pred=ypred)
 
     # ROC-AUC
-    fpr, tpr, roc_threshold = roc_curve(y_true = ytrue, y_score = ypred_proba)
-    roc_auc = roc_auc_score(y_true = ytrue, y_score = ypred_proba)
+    fpr, tpr, roc_threshold = roc_curve(y_true=ytrue, y_score=ypred_proba)
+    roc_auc = roc_auc_score(y_true=ytrue, y_score=ypred_proba)
 
     # Combine all
     all_score = {
@@ -54,10 +59,10 @@ def model_evaluation(ytrue: np.array, ypred: np.array, ypred_proba: np.array):
         "Micro Recall": micro_recall,
         "Accuracy": accuracy,
         "ROC-AUC": roc_auc,
-        "fpr": fpr.tolist(),
-        "tpr": tpr.tolist(),
-        "True_value": ytrue.tolist(),
-        "Predict_value": ypred.tolist(),
-        "Predict_prob_value": ypred_proba.tolist()
+        "fpr": fpr.tolist() if type(fpr) == np.ndarray else fpr,
+        "tpr": tpr.tolist() if type(tpr) == np.ndarray else tpr,
+        "True_value": ytrue.tolist() if type(ytrue) == np.ndarray else ytrue,
+        "Predict_value": ypred.tolist() if type(ypred) == np.ndarray else ypred,
+        "Predict_prob_value": ypred_proba.tolist() if type(ypred_proba) == np.ndarray else ypred_proba,
     }
     return all_score
