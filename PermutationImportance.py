67ce42e964cb192ddd0a307432402a1e614bc216
Jian-An Wang <facebook0983481181@gmail.com>
2023-04-14 08:19:01 +0800

    feat: add multi-class classification importance

--- PermutationImportance.py
+++ PermutationImportance.py
@@ -41,21 +41,20 @@ class permutation_importance:
             self.inputFeatures = self.model.feature_name_
         except:
             self.inputFeatures = self.model.feature_names_in_
-        self.inputFeatures = [i for i in self.inputFeatures if i != target]
+        self.inputFeatures = [i for i in self.inputFeatures if i != target] # 輸入模型使用的特徵
         
         self.originalTarget = self.disturbData[self.target].tolist()
-        self.originalYhat = self.model.predict(self.disturbData[self.inputFeatures])
+#         self.originalYhat = self.model.predict(self.disturbData[self.inputFeatures])
 
-        self.disturbFeature = self.inputFeatures.copy() if disturbFeature == "All" else disturbFeature
+        self.disturbFeature = self.inputFeatures.copy() if disturbFeature == "All" else disturbFeature # 想要打亂的特徵
         self.n_repeats = n_repeats
         self.metric = "F1-Score_for_1" if metric == "f1" else metric
-
         return
 
-    def permuteData(self, data, oneFeature):
-        assert (
-            oneFeature in self.inputFeatures
-        ), "The feature interested in disturbition must be the input features. "
+    def permuteData(self, data, oneFeature): # 確定留下來
+#         assert (
+#             oneFeature in self.inputFeatures
+#         ), "The feature interested in disturbition must be the input features. "
 
         permuteedData = data.copy()
         targetFeatureData = permuteedData[oneFeature].values.copy()
@@ -63,16 +62,20 @@ class permutation_importance:
         permuteedData[oneFeature] = targetFeatureData
         return permuteedData
 
-    def predict_disturb_data(self, oneFeature):
+    def predict_disturb_data(self, oneFeature): # 確定留下來
         permutedData = self.permuteData(data=self.disturbData, oneFeature=oneFeature)
-        if self.mlFlow is not None:
+        if self.mlFlow is not None: # 針對原始資料做解釋
             permutedData = self.mlFlow.transform_Pipeline(permutedData, mode = "test")
+#         print(permutedData.columns, self.inputFeatures)
         yhat = self.model.predict(permutedData[self.inputFeatures]).tolist()
-        if self.targetType == "classification":
+        if self.targetType == "classification" and permutedData[self.target].unique().tolist().__len__() == 2:
             yhat_proba = self.model.predict_proba(permutedData[self.inputFeatures])[
                 :, -1
             ].tolist()
             return {"yhat": yhat, "yhat_proba": yhat_proba}
+        elif self.targetType == "classification" and permutedData[self.target].unique().tolist().__len__() > 2:
+            yhat_proba = self.model.predict_proba(permutedData[self.inputFeatures]).tolist()
+            return {"yhat": yhat, "yhat_proba": yhat_proba}            
         else:
             return {"yhat": yhat}
 
@@ -90,7 +93,7 @@ class permutation_importance:
         if (
             self.targetType == "classification"
             and np.unique(self.originalTarget).tolist().__len__() == 2
-        ):
+        ): # 如果是二分類
             result = [
                 {
                     "Feature": oneResult["Feature"],
@@ -98,7 +101,7 @@ class permutation_importance:
                         two_class_model_evaluation.model_evaluation(
                             ytrue=self.originalTarget,
                             ypred=i["yhat"],
-                            ypred_proba=i["yhat"],
+                            ypred_proba=i["yhat_proba"],
                         )[self.metric]
                         for i in oneResult["DisturbYhat"]
                     ],
@@ -121,20 +124,58 @@ class permutation_importance:
                         - self.originalResult[self.metric],
                         popmean=0,
                     )[1],
-                    "originalMetric": self.originalResult[self.metric],
-                    "Metric_for_Each_Data": oneResult["Importance_for_Each_Data"],
-                    "Importance_for_Each_Data": np.array(
+                    "originalMetric": round(self.originalResult[self.metric], 4),
+                    "Metric_for_Each_Data": [round(i, 4) for i in oneResult["Importance_for_Each_Data"]],
+                    "Importance_for_Each_Data": [round(i) for i in (np.array(
                         oneResult["Importance_for_Each_Data"]
                     )
-                    - self.originalResult[self.metric],
+                    - self.originalResult[self.metric])],
                 }
                 for oneResult in result
             ]
         elif (
             self.targetType == "classification"
-            and np.unique(self.originalTarget).tolist().__len__()
-        ):
-            pass
+            and np.unique(self.originalTarget).tolist().__len__() > 2
+        ): # 如果是多分類
+            result = [
+                {
+                    "Feature": oneResult["Feature"],
+                    "Importance_for_Each_Data": [
+                        multi_class_model_evaluation.model_evaluation(
+                            ytrue=self.originalTarget,
+                            ypred=i["yhat"],
+                            ypred_proba=i["yhat_proba"],
+                        )[self.metric]
+                        for i in oneResult["DisturbYhat"]
+                    ],
+                }
+                for oneResult in result
+            ]
+            result = [
+                {
+                    "Feature": oneResult["Feature"],
+                    "Mean-Importance": np.mean(
+                        np.array(oneResult["Importance_for_Each_Data"])
+                        - self.originalResult[self.metric]
+                    ),
+                    "Std-Importance": np.std(
+                        np.array(oneResult["Importance_for_Each_Data"])
+                        - self.originalResult[self.metric]
+                    ),
+                    "p-value for t-test": ttest_1samp(
+                        a=np.array(oneResult["Importance_for_Each_Data"])
+                        - self.originalResult[self.metric],
+                        popmean=0,
+                    )[1],
+                    "originalMetric": round(self.originalResult[self.metric], 4),
+                    "Metric_for_Each_Data": [round(i, 4) for i in oneResult["Importance_for_Each_Data"]],
+                    "Importance_for_Each_Data": [round(i, 4) for i in (np.array(
+                        oneResult["Importance_for_Each_Data"]
+                    )
+                    - self.originalResult[self.metric])],
+                }
+                for oneResult in result
+            ]
         else:
             pass
         return result
